71c8bd1b1921870b3406e6f3900f9a39bc0faab2
Jose Paulino <jpaulino04@msn.com>
2019-03-29 22:38:08 -0400

    sync

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -1,24 +1,26 @@
 import React, { Component } from 'react'
 
-
 class Landing extends Component {
     constructor(props){
       super(props)
       this.state = {} 
+
     }
-    
+
     render() {    
       const settings = {
-        dots: false,
+        dots: true,
         infinite: true,
-        speed: 300,
-        autoplay: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1
         
       };
 
       return (
-        <div className="row col m-0">            
-            <iframe className="mx-auto"  width="620" height="420" src="https://www.youtube.com/embed/esDigDrvIwk" frameborder="0" allow="accelerometer;   autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>
+
+        <div className="row col m-0">
+            <iframe className="mx-auto"  width="620" height="420" src="https://s3.amazonaws.com/oneypaulino-rsized/oneypaulino-vids4-conveted.mp4" frameborder="0" allow="accelerometer;   autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>
             </iframe>
         </div>
       )
