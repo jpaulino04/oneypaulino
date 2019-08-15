d610d4412934640eb43f74af80415824be8359b2
Jose Paulino <jpaulino04@msn.com>
2019-08-15 10:32:36 -0400

    added one entry to front accordion

--- client/src/components/Accordion.js
+++ client/src/components/Accordion.js
@@ -14,34 +14,10 @@ export default function Accordion() {
                     </div>
                     <div id="collapseOne" className="collapse show" data-parent="#accordion">
                         <div className="card-body text-center text-muted-accordion">
-                        Oney Paulino mas que un artista, es un projecto con una visión innovadora. La Casa 112 se refiere a nuestra agrupacion. El termino 'La Casa' nos emotiva por que nos hace pensar que somos una familia. 
+                        Oney Paulino, mas que un artista, es un projecto con una visión innovadora. La Casa 112 se refiere a nuestra agrupación. El termino 'La Casa' nos emotiva por que nos hace pensar que somos una familia. 
                         </div>
                     </div>
-                    </div>
-                    <div className="card">
-                    <div className="card-header">
-                        <a className="collapsed card-link" data-toggle="collapse" href="#collapseTwo">
-                        Collapsible Group Item #2
-                    </a>
-                    </div>
-                    <div id="collapseTwo" className="collapse" data-parent="#accordion">
-                        <div className="card-body">
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                        </div>
-                    </div>
-                    </div>
-                    <div className="card">
-                    <div className="card-header">
-                        <a className="collapsed card-link" data-toggle="collapse" href="#collapseThree">
-                        Collapsible Group Item #3
-                        </a>
-                    </div>
-                    <div id="collapseThree" className="collapse" data-parent="#accordion">
-                        <div className="card-body">
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                        </div>
-                    </div>
-                    </div>
+                    </div>  
                 </div>
                 </div>
         </Fragment>
