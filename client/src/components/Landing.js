338cb34c191cc48336a0b6911063e851660dbc9b
Jose Paulino Romero <jpaulino04@msn.com>
2020-01-01 21:31:07 -0500

    added spotify follow button

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -23,10 +23,29 @@ class Landing extends Component {
           
         <Accordion />
 
+        <div className="row mt-4 mb-0 p-0 mx-auto text-center">
+          <div className="col-sm-8 mx-auto">
+            <iframe 
+              className="mx-auto d-block mb-0 text-center"
+              src="https://embed.spotify.com/follow/1/?uri=spotify:artist:2qlwghSoPrDUq6HixTnUd0&size=detail&theme=light" 
+              width="200" height="66" scrolling="no" frameborder="0"  allowtransparency="true">              
+            </iframe>            
+          </div>
+
+          <div className="col-sm-8 mx-auto mt-0 pt-0">
+          <p className="mx-auto text-muted d-block">Siguenos en Spotify</p>
+          </div>
+                    
+        </div>
+
+        {/* <div className="row col-sm-8 mt-0  mb-0 p-0 mx-auto text-center">
+          <p className="mx-auto muted d-block">Siguenos en Spotify</p>
+        </div> */}
+
         <div className="row col-sm-8 mt-4 mb-0 pb-0 mx-auto text-center">
           <p className="d-block mx-auto text-muted">
             <span className="text-warning"><b>Lo Nuevo 2020</b></span>, <u><a className="text-dark " href="https://www.youtube.com/embed/uzP1QQEsOS4">No Creo en Cupido!</a></u></p>
-        </div>
+        </div>    
 
         <div className="row mx-0 mt-0 col-12 mb-4 pb-2">          
           <iframe 
